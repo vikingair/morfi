91c6e1eac2311daaa3e65fd7a683daff2bd93f8f
Viktor Luft <viktor.luft@freiheit.com>
2018-03-01 08:47:16 +0100

    Upcoming test utility: first experiments

--- src/js/AsyncValidationSample.test.js
+++ src/js/AsyncValidationSample.test.js
@@ -9,107 +9,50 @@
 import React from 'react';
 import AsyncValidationSample from './AsyncValidationSample';
 import { Spy } from 'spy4js';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import { Input } from './fields/FormInput';
+import { FormWrapper } from '../test/form-test-util';
 
 describe('AsyncValidationSample', () => {
     it('renders the initial form', () => {
         expect(toJson(shallow(<AsyncValidationSample />))).toMatchSnapshot();
     });
-    it('knows the requirements', () => {
-        const mounted = mount(<AsyncValidationSample />);
 
-        const inputs = mounted.find(Input);
-        expect(inputs.length).toBe(2);
+    it('validates the form integration', async () => {
+        const wrapper = new FormWrapper(<AsyncValidationSample />)
+            .validate(
+                { base: 'state', name: 'data' },
+                { name: 'userName', component: Input, required: true },
+                { name: 'realName', component: Input, required: false }
+            )
+            .focus('realName')
+            .change('Tester')
+            .valueIs('Tester')
+            .errorIs(undefined)
+            .change('Tester @ga!n')
+            .valueIs('Tester @ga!n')
+            .errorIs({ id: 'AsyncValidationSample.realName.validation.requirements' })
+            .focus('userName')
+            .change('TestFoo')
+            .errorIs(undefined);
 
-        const [userNameInput, realNameInput] = inputs;
+        const mathRandomSpy = Spy.on(Math, 'random').returns(0.9);
+        await wrapper.blur().nextTick();
 
-        expect(userNameInput.props.required).toBeTruthy();
-        expect(userNameInput.props.value).toBe(undefined);
-        expect(userNameInput.props.error).toBe(undefined);
-        expect(userNameInput.props.label).toBe('Username');
-        expect(userNameInput.props.disabled).toBeFalsy();
-        expect(userNameInput.props.pending).toBeFalsy();
+        wrapper
+            .focus('userName')
+            .valueIs('TestFoo')
+            .errorIs({ id: 'AsyncValidationSample.userName.already.registered', values: { userName: 'TestFoo' } })
+            .change('otherFooName')
+            .errorIs(undefined);
 
-        expect(realNameInput.props.required).toBeFalsy();
-        expect(realNameInput.props.value).toBe(undefined);
-        expect(realNameInput.props.error).toBe(undefined);
-        expect(realNameInput.props.label).toBe('Real name');
-        expect(realNameInput.props.disabled).toBeFalsy();
-    });
+        mathRandomSpy.returns(0.1);
+        await wrapper.blur().nextTick();
 
-    it('validates the real name', () => {
-        const mounted = mount(<AsyncValidationSample />);
-
-        const realNameInput = () => mounted.find(Input).at(1);
-
-        realNameInput()
-            .find('input')
-            .simulate('change', { target: { value: 'No r34l N@me' } });
-
-        const nextProps = realNameInput().props();
-        expect(nextProps.value).toBe('No r34l N@me');
-        expect(nextProps.error).toEqual({ id: 'AsyncValidationSample.realName.validation.requirements' });
-    });
-
-    it('validates the user name', async () => {
-        const mounted = mount(<AsyncValidationSample />);
-
-        // always take the latest version of the mounted instance
-        const userNameInput = () => mounted.find(Input).at(0);
-
-        // change the input to an invalid value
-        userNameInput()
-            .find('input')
-            .simulate('change', { target: { value: '' } });
-
-        // see the error
-        let nextProps = userNameInput().props();
-        expect(nextProps.value).toBe('');
-        expect(nextProps.error).toEqual({ id: 'validation.characters.atLeast.x', values: { num: 1, s: '' } });
-
-        // refresh the state and let the "call" succeed
-        mounted.setState({ data: { values: {}, errors: {} } });
-        const mathRandomSpy = Spy.on(Math, 'random').returns(0.1);
-
-        // leave the field with some entered value
-        userNameInput()
-            .find('input')
-            .simulate('blur', { target: { value: 'Anything' } });
-
-        // wait for the validations
-        await window.nextTick();
-
-        // update the component, since the setState was called async and ezyme did not update correctly
-        mounted.update();
-
-        // no error is displayed
-        nextProps = userNameInput().props();
-        expect(nextProps.value).toBe('Anything');
-        expect(nextProps.error).toEqual(undefined);
-
-        // refresh the state and let the "call" fail
-        mounted.setState({ data: { values: {}, errors: {} } });
-        mathRandomSpy.returns(0.9);
-
-        // leave the field with some entered value
-        userNameInput()
-            .find('input')
-            .simulate('blur', { target: { value: 'Anything else' } });
-
-        // wait for the validations
-        await window.nextTick();
-
-        // update the component, since the setState was called async and ezyme did not update correctly
-        mounted.update();
-
-        // see the error
-        nextProps = userNameInput().props();
-        expect(nextProps.value).toBe('Anything else');
-        expect(nextProps.error).toEqual({
-            id: 'AsyncValidationSample.userName.already.registered',
-            values: { userName: 'Anything else' },
-        });
+        wrapper
+            .focus('userName')
+            .valueIs('otherFooName')
+            .errorIs(undefined);
     });
 });
---
+++ src/test/form-test-util.js
@@ -0,0 +1,208 @@
+/**
+ * This file is part of form4react which is released under MIT license.
+ *
+ * The LICENSE file can be found in the root directory of this project.
+ *
+ * @flow
+ */
+
+import { Form, Field } from '../js/form';
+import { mount } from 'enzyme';
+import type { ReactWrapper } from 'enzyme';
+import type { ErrorMessage } from '../js/form';
+
+type FieldSpec = {
+    name: string,
+    component: any,
+    required?: boolean,
+};
+
+const _makeTestData = (...fieldSpecs: FieldSpec[]): Object => {
+    const result = { values: {}, errors: {} };
+    for (let key = 0; key < fieldSpecs.length; key++) {
+        const name = fieldSpecs[key].name;
+        result.values[name] = 'test.value.' + name;
+        result.errors[name] = { id: 'test.error.' + name };
+    }
+    return result;
+};
+
+const _findWithin = (fields: ReactWrapper, name: string): ReactWrapper | void => {
+    const found = [];
+    for (let index = 0; index < fields.length; index++) {
+        const field = fields.at(index);
+        if (field.props().name === name) {
+            found.push(field);
+        }
+    }
+    if (found.length !== 1) {
+        throw new Error('Did not find exactly one field with name: ' + name);
+    }
+    return found[0];
+};
+
+const NOP = () => {};
+
+const _findTypeFromField = (fieldWrapper: ReactWrapper): any => {
+    return fieldWrapper.props().children({ onChange: NOP, onBlur: NOP, required: false }).type;
+};
+
+// first version of the upcoming test util
+export class FormWrapper {
+    +_mounted: ReactWrapper;
+    focused: { name: string, component: any } | void;
+
+    constructor(nodes: React$Node) {
+        this._mounted = mount(nodes);
+        const forms = this._mounted.find(Form);
+        if (forms.length !== 1) {
+            throw new Error('Did not find exactly one form.');
+        }
+        const fields = this._mounted.find(Field);
+        if (fields.length === 0) {
+            throw new Error('There were no fields within the form.');
+        }
+        if (forms.at(0).find(Field).length !== fields.length) {
+            throw new Error('Found fields outside the form.');
+        }
+    }
+
+    blur = (): FormWrapper => {
+        if (!this.focused) {
+            return this;
+        }
+        const { name, component } = this.focused;
+        const field = this.find(name);
+        if (!field) {
+            return this;
+        }
+        const comp = field.find(component);
+        if (comp.length !== 1) {
+            throw new Error('Did not find exactly one component for field with name: ' + name);
+        }
+        const props = comp.props();
+        const currentValue = props.value;
+        if (props.onBlur) {
+            props.onBlur(currentValue);
+            this._mounted.update();
+        }
+        this.focused = undefined;
+        return this;
+    };
+
+    nextTick = async () => {
+        await new Promise(resolve => process.nextTick(resolve));
+        this._mounted.update();
+    };
+
+    focus = (name: string): FormWrapper => {
+        this.blur();
+        const field = this.find(name);
+        if (!field) {
+            throw new Error(`Field with name ${name} does not exist.`);
+        }
+        const component = _findTypeFromField(field);
+        const comp = field.find(component);
+        if (comp.length !== 1) {
+            throw new Error('Did not find exactly one component for field with name: ' + name);
+        }
+        this.focused = { name, component };
+        return this;
+    };
+
+    find = (name: string): ReactWrapper | void => {
+        return _findWithin(this._mounted.find(Field), name);
+    };
+
+    validate = (
+        location?: { base: 'state' | 'props', name: string } = {
+            base: 'state',
+            name: 'data',
+        },
+        ...fieldSpecs: FieldSpec[]
+    ): FormWrapper => {
+        const data = _makeTestData(...fieldSpecs);
+        const oldData =
+            location.base === 'state' ? this._mounted.state()[location.name] : this._mounted.props()[location.name];
+        location.base === 'state'
+            ? this._mounted.setState({ [location.name]: data })
+            : this._mounted.setProps({ [location.name]: data });
+        const fields = this._mounted.find(Field);
+        for (let key = 0; key < fieldSpecs.length; key++) {
+            const fieldSpec = fieldSpecs[key];
+            const name = fieldSpec.name;
+            const field = _findWithin(fields, name);
+            if (!field) {
+                throw new Error(`Field with name "${name}" could not be found.`);
+            }
+            const component = field.find(fieldSpec.component);
+            if (component.length !== 1) {
+                throw new Error('Did not find exactly one component for field with name: ' + name);
+            }
+            const props = component.props();
+            expect(props.value).toBe(data.values[name]);
+            expect(props.error).toEqual(data.errors[name]);
+            if (fieldSpec.required !== undefined) {
+                expect(props.required).toBe(fieldSpec.required);
+            }
+        }
+        location.base === 'state'
+            ? this._mounted.setState({ [location.name]: oldData })
+            : this._mounted.setProps({ [location.name]: oldData });
+        return this;
+    };
+
+    change = (value: string | boolean | number): FormWrapper => {
+        if (!this.focused) {
+            throw new Error('No field was focused');
+        }
+        const { name, component } = this.focused;
+        const field = this.find(name);
+        if (!field) {
+            throw new Error('Focused field does not exist anymore');
+        }
+        const comp = field.find(component);
+        if (comp.length !== 1) {
+            throw new Error('Did not find exactly one component for field with name: ' + name);
+        }
+        comp.props().onChange(value);
+        this._mounted.update();
+        return this;
+    };
+
+    valueIs = (value: any): FormWrapper => {
+        if (!this.focused) {
+            throw new Error('No field was focused');
+        }
+        const { name, component } = this.focused;
+        const field = this.find(name);
+        if (!field) {
+            throw new Error('Focused field does not exist anymore');
+        }
+        const comp = field.find(component);
+        if (comp.length !== 1) {
+            throw new Error('Did not find exactly one component for field with name: ' + name);
+        }
+        const props = comp.props();
+        expect(props.value).toEqual(value);
+        return this;
+    };
+
+    errorIs = (error: ErrorMessage | void): FormWrapper => {
+        if (!this.focused) {
+            throw new Error('No field was focused');
+        }
+        const { name, component } = this.focused;
+        const field = this.find(name);
+        if (!field) {
+            throw new Error('Focused field does not exist anymore');
+        }
+        const comp = field.find(component);
+        if (comp.length !== 1) {
+            throw new Error('Did not find exactly one component for field with name: ' + name);
+        }
+        const props = comp.props();
+        expect(props.error).toEqual(error);
+        return this;
+    };
+}
