45ab8e128c371960d0e6afdf0bf5d6f7358efb5f
Viktor Luft <viktor.luft@freiheit.com>
2018-05-23 15:19:17 +0200

    Fixed console logging

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -54,19 +54,19 @@ function registerValidSW(swUrl) {
                             // the fresh content will have been added to the cache.
                             // It's the perfect time to display a "New content is
                             // available; please refresh." message in your web app.
-                            console.log('New content is available; please refresh.');
+                            window.console.log('New content is available; please refresh.');
                         } else {
                             // At this point, everything has been precached.
                             // It's the perfect time to display a
                             // "Content is cached for offline use." message.
-                            console.log('Content is cached for offline use.');
+                            window.console.log('Content is cached for offline use.');
                         }
                     }
                 };
             };
         })
         .catch(error => {
-            console.error('Error during service worker registration:', error);
+            window.console.error('Error during service worker registration:', error);
         });
 }
 
@@ -88,7 +88,7 @@ function checkValidServiceWorker(swUrl) {
             }
         })
         .catch(() => {
-            console.log('No internet connection found. App is running in offline mode.');
+            window.console.log('No internet connection found. App is running in offline mode.');
         });
 }
 
