0a7dda390826c7d955e75c7a96673b74c83747d9
Viktor Luft <viktor.luft@freiheit.com>
2019-03-25 09:02:11 +0100

    Fixed test script

--- bin/test.sh
+++ bin/test.sh
@@ -10,14 +10,19 @@ CYAN='\033[0;36m'
 NC='\033[0m'
 
 echo ""
+echo -e "Running ${CYAN}distribution${NC}:"
+echo ""
+yarn run dist
+
+echo ""
 echo -e "Running ${CYAN}flow${NC}:"
 echo -e "${GREEN}"
-npm --silent --dry-run=true run flow
+yarn --silent run flow
 
 echo ""
 echo -e "${NC}"
 echo -e "Running ${CYAN}eslint${NC}:"
-npm --silent run eslint
+yarn --silent run eslint
 echo ""
 echo -e "${GREEN}No errors!${NC}"
 
@@ -25,6 +30,6 @@ echo ""
 echo ""
 echo -e "Running ${CYAN}tests${NC}:"
 echo ""
-CI=true npm --silent run test -- --coverage
+CI=true yarn --silent run test --coverage
 
 popd &>/dev/null
