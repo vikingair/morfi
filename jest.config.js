93938b0367717a4bb5bf39423815a1cf7607321c
Viktor Luft <viktor.luft@freiheit.com>
2019-06-06 09:02:40 +0200

    Updated all dev dependencies with security issues

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
     setupFilesAfterEnv: ['<rootDir>/test_resources/setupTests.js'],
     coverageThreshold: { global: { statements: 100, branches: 100, functions: 100, lines: 100 } },
     coverageDirectory: '<rootDir>/build/coverage',
-    coverageReporters: ['lcov'],
+    coverageReporters: ['lcov', 'text'],
     moduleNameMapper: { '\\.(scss)$': 'identity-obj-proxy', '(.*)/(dist/cjs/)': '$1/src/' },
 };
