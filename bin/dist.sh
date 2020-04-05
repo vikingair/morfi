51d29c5f3b8529a7f48d628c49165ceee3f76e65
Viktor Luft <viktor.luft@freiheit.com>
2020-04-05 11:23:01 +0200

    Fix sed usage for both different os types

--- bin/dist.sh
+++ bin/dist.sh
@@ -10,8 +10,19 @@ yarn run rollup -c
 
 # now copy some files for distribution of morfi-test-utils
 cp -r morfi-test-utils/src morfi-test-utils/npm/src
-# modify the import to use the external peer dependency morfi
-sed -i '' "s|'\.\.\/\.\.\/src'|'morfi'|" morfi-test-utils/npm/src/index.js
-# fix incorrect flow paths
-sed -i '' "s|\.\.\/src|src|" morfi-test-utils/npm/dist/cjs/index.js.flow
-sed -i '' "s|\.\.\/src|src|" morfi-test-utils/npm/dist/esm/index.js.flow
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # modify the import to use the external peer dependency morfi
+    sed -i '' "s|'\.\.\/\.\.\/src'|'morfi'|" morfi-test-utils/npm/src/index.js
+    # fix incorrect flow paths
+    sed -i '' "s|\.\.\/src|src|" morfi-test-utils/npm/dist/cjs/index.js.flow
+    sed -i '' "s|\.\.\/src|src|" morfi-test-utils/npm/dist/esm/index.js.flow
+else
+    # modify the import to use the external peer dependency morfi
+    sed -i "s|'\.\.\/\.\.\/src'|'morfi'|" morfi-test-utils/npm/src/index.js
+    # fix incorrect flow paths
+    sed -i "s|\.\.\/src|src|" morfi-test-utils/npm/dist/cjs/index.js.flow
+    sed -i "s|\.\.\/src|src|" morfi-test-utils/npm/dist/esm/index.js.flow
+fi
+
+
