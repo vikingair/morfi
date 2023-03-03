90975b18011999feb9bbe0a1a83100674ba51340
Viktor Luft <viktor.luft@freiheit.com>
2023-03-03 08:58:48 +0100

    Fix some issues (#21)
    * Fix some issues
    
    * Do not export internal func
    
    * Prevent minification of library code

--- docs_src/src/hooks/useChangeEffect.ts
+++ docs_src/src/hooks/useChangeEffect.ts
@@ -1,13 +1,25 @@
 import { DependencyList, useEffect, useRef } from 'react';
 
+// React strict mode calls all effects twice, so we need this helper
+// see: https://github.com/streamich/react-use/blob/master/src/useFirstMountState.ts
+export const useFirstMountState = (): boolean => {
+    const isFirst = useRef(true);
+
+    if (isFirst.current) {
+        isFirst.current = false;
+        return true;
+    }
+
+    return isFirst.current;
+};
+
 type OptionalCleanUpCb = void | (() => void);
-export const useChangeEffect: typeof useEffect = (create: () => OptionalCleanUpCb, deps?: DependencyList) => {
-    const firstRun = useRef<boolean>(true);
+export const useChangeEffect: typeof useEffect = (effect: () => OptionalCleanUpCb, deps?: DependencyList) => {
+    const isFirst = useFirstMountState();
+
     useEffect(() => {
-        if (firstRun.current) {
-            firstRun.current = false;
-        } else {
-            return create();
+        if (!isFirst) {
+            return effect();
         }
     }, deps); // eslint-disable-line react-hooks/exhaustive-deps
 };
